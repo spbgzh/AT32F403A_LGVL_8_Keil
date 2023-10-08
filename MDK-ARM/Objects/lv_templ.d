105be1ed0e7cec098d36f23f52034d78d91be351
spbgzh <72414103+spbgzh@users.noreply.github.com>
2023-10-08 12:33:34 +0300

    update

---
+++ MDK-ARM/Objects/lv_templ.d
@@ -0,0 +1,5 @@
+./objects/lv_templ.o: \
+  C:\Users\zjjhg\AppData\Local\Arm\Packs\LVGL\lvgl\8.3.6\src\misc\lv_templ.c \
+  RTE\LVGL\lv_conf_cmsis.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  RTE\_Target_1\RTE_Components.h RTE\_Target_1\Pre_Include_Global.h
---
+++ MDK-ARM/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,30 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'template' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "at32f403a_407.h"
+
+/* LVGL.LVGL::LVGL:lvgl:Benchmark:8.3.6 */
+/*! \brief enable demo:bencharmk */
+#define LV_USE_DEMO_BENCHMARK         1
+/* LVGL.LVGL::LVGL:lvgl:Essential:8.3.6 */
+/*! \brief Enable LVGL */
+#define RTE_GRAPHICS_LVGL
+/* LVGL.LVGL::LVGL:lvgl:Extra Themes:8.3.6 */
+/*! \brief use extra themes, widgets and layouts */
+#define RTE_GRAPHICS_LVGL_USE_EXTRA_THEMES
+
+
+#endif /* RTE_COMPONENTS_H */
